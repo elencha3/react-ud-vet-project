1deaad8503c63bb52a21b48449978e436bf82565
Esther Leñador <e.lenadorc@hotmail.com>
2021-12-21 20:06:44 +0100

    change title header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
             <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento Pacientes {""}
-                <span className='text-cyan-400'>Veterinaria</span>
+                <span className='text-cyan-400'>Veterinaria 2021</span>
             </h1>
         </>
     )
